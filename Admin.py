d7f501746769ade45cff42fd500e51e6d2a5e2a2
jordan-hess <jchno116012003@gmail.com>
2021-07-09 18:05:01 +0200

    upgrade

--- Admin.py
+++ Admin.py
@@ -1,63 +1,39 @@
-import datetime
+
 from tkinter import *
 
-
 root = Tk()
-root.title("Login")
+root.title("Admin Login")
 from tkinter import messagebox
+root.geometry("500x400")
 
 # usernames and passwords
-userList = ["jordan", "godwin"]
-passwList = ["123", "closethelaptops"]
-
-frame3 = Frame(root, bd=2, width=200, bg="yellow")
-frame3.grid(row=0, column=1)
-
-frame5 = Frame(root, bd=2, bg="yellow")
-frame5.grid(row=1, column=1)
+userList = "admin"
+passwList = "adim123"
 
 
 def login():
-    global loginUsername
-    global loginPassword
-    global IdNumber
-    if loginUsername.get() in userList and loginPassw.get() in passwList:
-        User = userList.index(loginUsername.get())
-        Passw = passwList.index(loginPassword.get())
-        Id_num = IdNumber.get()
-        int(loginPassword.get())
-        if User == Passw:
-            frame3.grid_forget()
-            frame5.grid_forget()
-            root.destroy()
-            import lotto
+        if loginUser == userList and passwList == passwList:
+            root.grid_forget()
+            root.grid_forget()
+            messagebox.showinfo(message="welcome")
         else:
-            messagebox.showerror(message="you are not old enough to play")
-
-    else:
-        messagebox.showerror(message="password or username is invalid, try again")
+         messagebox.showerror(message="password or username is invalid, try again")
 
 
-loginLabelUser = Label(frame3, text="Log in:", bg="yellow")
-loginLabelUser.pack(anchor=W)
+loginLabelUser = Label(root, text="Log in:").place(x=50, y=100)
 
 loginUser = StringVar()
-loginUsername = Entry(frame3, textvariable=loginUser, width=40)
-loginUsername.pack(anchor=W)
+loginUsername = Entry(root, textvariable=loginUser, width=40)
+loginUsername.place(x=150, y=100)
 
-loginLabelPassw = Label(frame3, text="Password:", bg="yellow")
-loginLabelPassw.pack(anchor=W)
+loginLabelPassw = Label(root, text="Password:")
+loginLabelPassw.place(x=50, y=150)
 
 loginPassw = StringVar()
-loginPassword = Entry(frame3, textvariable=loginPassw, width=40)
-loginPassword.pack()
-
+loginPassword = Entry(root, textvariable=loginPassw, width=40)
+loginPassword.place(x=150, y=150)
 
 # button to login, this will start the function login()
-button = Button(frame3, text="Continue...", command=login)
-button.pack()
-
-# Checking age by using ID
-
+button = Button(root, text="Continue...", command=login).place(x=150, y=200)
 
 root = mainloop()
